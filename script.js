5b3bfc3a8aa1f06c10df02aaf27a74756ddbf9a2
Jonatas Ferreira <jonferreira.psd@gmail.com>
2026-02-16 17:01:46 -0300

    Funcionalidade: Função que exibe os números sorteados em tela com a animação

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const form = document.querySelector("form");
 const amountInput = document.getElementById("amount");
 const initialInput = document.getElementById("initial");
 const finalInput = document.getElementById("final");
+const noRepeatCheckbox = document.getElementById("checkbox");
+
+const drawingScreen = document.querySelector(".drawing-screen");
+const dataDraw = document.querySelector(".data-draw");
+const resultScreen = document.querySelector(".result-screen");
+const titleResult = document.querySelector(".title-result");
 
 // percorre todos os inputs da lista
 dataInput.forEach((input) => {
@@ -18,20 +24,20 @@ dataInput.forEach((input) => {
 form.onsubmit = (event) => {
   event.preventDefault(); // impede o carregamento de envio do formulário
 
-  validateForm(); // chama a função de validação do formulário
+  // chama a função de validação do formulário e, se retornar false, interrompe a execução
+  if (!validateForm()) {
+    return;
+  }
 
-  console.log(
-    generateNumbers(
-      parseInt(amountInput.value),
-      parseInt(initialInput.value),
-      parseInt(finalInput.value),
-    ),
-    generateRandomNumbers(
-      parseInt(amountInput.value),
-      parseInt(initialInput.value),
-      parseInt(finalInput.value),
-    ),
-  );
+  // converte os valores dos inputs para números inteiros
+  const amount = parseInt(amountInput.value);
+  const initial = parseInt(initialInput.value);
+  const final = parseInt(finalInput.value);
+
+  const resultNumbers = generateNumbers(amount, initial, final);
+
+  // chama a função para exibir os números sorteados
+  displayNumbers(resultNumbers);
 };
 
 // função para mostrar a mensagem de alerta de erro
@@ -81,8 +87,8 @@ function validateForm() {
   }
 }
 
-// função para gerar números com repetição
-function generateNumbers(amount, initial, final) {
+// função para gerar números aleatórios com repetição;
+function generateRandomNumbers(amount, initial, final) {
   const numbers = [];
   const range = final - initial + 1;
   for (let i = 0; i < amount; i++) {
@@ -92,16 +98,14 @@ function generateNumbers(amount, initial, final) {
   return numbers;
 }
 
-// função para gerar números sem repetição
-function generateRandomNumbers(amount, initial, final) {
-  const noRepeatCheckbox = document.getElementById("checkbox");
-
+// função para gerar números aleatórios sem repetição;
+function generateNumbers(amount, initial, final) {
   if (noRepeatCheckbox.checked) {
     const numbers = new Set();
     const range = final - initial + 1;
     if (amount > range) {
       showAlertMessage(
-        "A quantidade de números solicitada excede o intervalo disponível.",
+        "A quantidade de números excede o intervalo disponível.",
       );
       return [];
     }
@@ -113,6 +117,97 @@ function generateRandomNumbers(amount, initial, final) {
 
     return Array.from(numbers);
   } else {
-    return generateNumbers(amount, initial, final);
+    return generateRandomNumbers(amount, initial, final);
+  }
+}
+
+let DrawNumber = 0; // variável para contar o número de sorteios realizados
+
+function displayNumbers(numbers) {
+  try {
+    dataDraw.classList.add("invisible"); // esconde a tela de formulario
+
+    resultScreen.classList.remove("invisible"); // mostra a tela de resultado
+    resultScreen.classList.add("grid");
+
+    resultScreen.innerHTML = ""; // limpa o conteúdo da tela de resultado para o próximo sorteio
+
+    // cria o título do resultado dinamicamente para cada sorteio
+    const titleResult = document.createElement("header");
+    titleResult.classList.add("title-result", "flex");
+    titleResult.innerHTML = `<h2>Resultado do sorteio:</h2>`;
+
+    const numberResults = document.createElement("p");
+    numberResults.textContent = `${DrawNumber + 1}° resultado`;
+    titleResult.appendChild(numberResults);
+    resultScreen.prepend(titleResult);
+
+    const numbersContainer = document.createElement("div");
+    numbersContainer.classList.add("number-container");
+    resultScreen.append(numbersContainer); // adiciona o container de números à tela de resultado
+
+    // adiciona o container de números ao container principal
+
+    // variável para controlar o índice do número a ser exibido
+    let indexNum = 0;
+
+    const showNumbers = () => {
+      // verifica se ainda há números para exibir
+      if (indexNum < numbers.length) {
+        // cria os elementos para exibir o número sorteado
+        const numberContent = document.createElement("div");
+        numberContent.classList.add("number-content");
+
+        const boxNumber = document.createElement("div");
+        boxNumber.classList.add("box-number");
+
+        const numberSorted = document.createElement("output");
+        numberSorted.classList.add("draw-result");
+        numberSorted.setAttribute("aria-live", "polite");
+        numberSorted.textContent = numbers[indexNum]; // exibe o número sorteado atual
+
+        numberContent.appendChild(boxNumber);
+        numbersContainer.append(numberContent);
+        numberContent.append(numberSorted);
+        indexNum++;
+        setTimeout(showNumbers, 3500); // Método que exibirá os números com um atraso para a animação
+      } else {
+        const buttonNewDraw = document.createElement("div");
+        buttonNewDraw.classList.add("button-new-draw");
+        const button = document.createElement("button");
+        const playIcon = document.createElement("img");
+        const circleIcon = document.createElement("img");
+        button.type = "submit";
+        button.textContent = "Sortear novamente";
+        playIcon.src = "/assets/play.svg";
+        playIcon.alt = "Play";
+        circleIcon.src = "/assets/circle.svg";
+        circleIcon.alt = "Circle";
+        buttonNewDraw.style.opacity = 0;
+
+        button.appendChild(playIcon);
+        button.appendChild(circleIcon);
+        buttonNewDraw.appendChild(button);
+        resultScreen.append(buttonNewDraw);
+
+        setTimeout(() => {
+          buttonNewDraw.style.opacity = 1;
+        }, 400);
+
+        button.onclick = () => {
+          // limpa a tela de resultado para o próximo sorteio
+          numbersContainer.innerHTML = "";
+          displayNumbers(numbers); // chama a função para exibir os números novamente
+        };
+      }
+    };
+
+    showNumbers(); // inicia a exibição dos números sorteado
+    DrawNumber++; // incrementa o número do sorteio para o próximo resultado
+  } catch (error) {
+    showAlertMessage(
+      "Ocorreu um erro ao exibir os números. Por favor, tente novamente.",
+    );
+    console.error("Erro ao exibir os números:", error);
   }
 }
