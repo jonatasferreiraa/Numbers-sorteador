4ee4e84cb4cab27d6268c15b13194e1814b964a9
Jonatas Ferreira <jonferreira.psd@gmail.com>
2026-02-16 12:14:56 -0300

    Funcionalidade: Observa o envio de formulário e previne o carregamento de página no envio do mesmo.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const dataInput = document.querySelectorAll(".input-wrapper input");
+const form = document.querySelector("form");
 
 // percorre todos os inputs da lista
-
 dataInput.forEach((input) => {
   // observa os valores digitados pelo usuário
   input.oninput = () => {
-    // pega o valor digitado e remove tudo que não for número
-    let value = input.value.replace(/\D/g, "");
-
-    // limita o valor a no máximo 3 caracteres e atualiza o input
-    input.value = value.slice(0, 3);
+    let value = input.value.replace(/\D/g, ""); // pega o valor digitado e remove tudo que não for número
+    input.value = value.slice(0, 3); // limita o valor a no máximo 3 caracteres e atualiza o input
   };
 });
+
+form.onsubmit = (event) => {
+  event.preventDefault(); // impede o carregamento de envio do formulário
+};
